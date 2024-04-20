ceac16ce1d5b33bfc3272af1c15badae4d5ebfbe
Ritesh <riteshsaini331@gmail.com>
2024-04-20 14:00:04 +0530

    assets set

--- src/config/constant.php
+++ src/config/constant.php
@@ -1,41 +1,43 @@
 <?php
 // Root Constant
-define('DS',DIRECTORY_SEPARATOR);
-define('BACK_DOT','..');
-define('SITE_ROOT',BACK_DOT.DS."src".DS);
+define('DS', DIRECTORY_SEPARATOR);
+define('BACK_DOT', '..');
+define('SITE_ROOT', BACK_DOT . DS . "src" . DS);
 
-define('APP_ROOT',SITE_ROOT."app.php");
+define('APP_ROOT', SITE_ROOT . "app.php");
 
-define('CONFIG_ROOT',SITE_ROOT."config".DS);
-define('CORE_ROOT',SITE_ROOT."core".DS);
-define('MODULES_ROOT',SITE_ROOT."modules".DS);
-define('ROUTES_ROOT',SITE_ROOT."routes".DS);
-define('SERVICES_ROOT',SITE_ROOT."services".DS);
-define('UTILS_ROOT',SITE_ROOT."utils".DS);
+define('CONFIG_ROOT', SITE_ROOT . "config" . DS);
+define('CORE_ROOT', SITE_ROOT . "core" . DS);
+define('MODULES_ROOT', SITE_ROOT . "modules" . DS);
+define('ROUTES_ROOT', SITE_ROOT . "routes" . DS);
+define('SERVICES_ROOT', SITE_ROOT . "services" . DS);
+define('UTILS_ROOT', SITE_ROOT . "utils" . DS);
+define('STATIC_PAGE_ROOT', UTILS_ROOT . "static-page");
 
 //Comman File Paths
-define('CONFIG_PATH',CONFIG_ROOT."config.php");
+define('CONFIG_PATH', CONFIG_ROOT . "config.php");
 
-    
+
 //Methods Allowed
-define("GET","GET");
-define("POST","POST");
+define("GET", "GET");
+define("POST", "POST");
 
 //Request & Respose Standard Keys
-define("STATUS_CODE",'status_code');
-define("DATA",'data');
-define("ERROR",'error');
-define("MSG",'msg');
-define("LOG",'log');
+define("STATUS_CODE", 'status_code');
+define("DATA", 'data');
+define("ERROR", 'error');
+define("MSG", 'msg');
+define("LOG", 'log');
+
+//Static Folder Path
+define("IMAGES_PATH", "assets" . DS . "img" . DS);
 
 //Standard File Paths
-define("PAGE_NOT_FOUND_404",'views/default/page_not_found.php');
-define("SERVER_DOWN",'views/default/server_down.php');
+define("PAGE_NOT_FOUND_404", STATIC_PAGE_ROOT . DS . 'page_not_found.php');
+define("SERVER_DOWN", 'views/default/server_down.php');
 
 //String Constants
-define("APP_TITLE","Afwaah");
+define("APP_TITLE", "Afwaah");
 
 //keys
-define("IS_ADMIN","is_admin");
-
-?>
+define("IS_ADMIN", "is_admin");
--- src/utils/static-page/page_not_found.php
+++ src/utils/static-page/page_not_found.php
@@ -1,5 +1,27 @@
 <?php
+echo <<<END
+<!DOCTYPE html>
+<html>
+<head>
+    <title>404 Not Found</title>
+    <style>
+        body {
+            text-align: center;
+            padding: 20px;
+        }
+        h1 {
+            font-size: 2em;
+        }
+        img {
+            height: 400px;
+        }
+    </style>
+</head>
+<body>
+    <img src='/assets/img/404.png' alt='404 Not Found'>
+    <h1>404 Not Found</h1>
+    <p>The requested URL was not found on this server.</p>
+</body>
+</html>
 
-
-
-?>
+END;
